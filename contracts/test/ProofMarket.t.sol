be92de5341c036406844d512daa2e8520d325ed1
Parker Thompson <parker@risczero.com>
2024-09-26 22:56:49 -0700

    Fixed unbalanced tree impl in contracts

--- contracts/test/ProofMarket.t.sol
+++ contracts/test/ProofMarket.t.sol
@@ -726,7 +726,16 @@ contract ProofMarketTest is Test {
         benchFulfillBatch(128);
     }
 
-    function testProcessTree() public {
+    function testProcessTree2() public {
+        bytes32[] memory leaves = new bytes32[](2);
+        leaves[0] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
+        leaves[1] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
+
+        bytes32 root = MerkleProofish.processTree(leaves);
+        assertEq(root, 0x5032880539b5d039d4a4a8042745c9ad14934c96b76d7e61ea03550e29b234af);
+    }
+
+    function testProcessTree3() public {
         bytes32[] memory leaves = new bytes32[](3);
         leaves[0] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
         leaves[1] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
