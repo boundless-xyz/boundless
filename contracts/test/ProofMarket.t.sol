31d0dabcd414d55ab65a9a9e20bf64c49bb4421e
Victor Graf <victor@risczero.com>
2024-09-27 11:06:52 -0700

    fix a couple of warnings from solc

--- contracts/test/ProofMarket.t.sol
+++ contracts/test/ProofMarket.t.sol
@@ -726,7 +726,7 @@ contract ProofMarketTest is Test {
         benchFulfillBatch(128);
     }
 
-    function testProcessTree2() public {
+    function testProcessTree2() public pure {
         bytes32[] memory leaves = new bytes32[](2);
         leaves[0] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
         leaves[1] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
@@ -735,7 +735,7 @@ contract ProofMarketTest is Test {
         assertEq(root, 0x5032880539b5d039d4a4a8042745c9ad14934c96b76d7e61ea03550e29b234af);
     }
 
-    function testProcessTree3() public {
+    function testProcessTree3() public pure {
         bytes32[] memory leaves = new bytes32[](3);
         leaves[0] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
         leaves[1] = 0x6a428060b5d51f04583182f2ff1b565f9db661da12ee7bdc003e9ab6d5d91ba9;
