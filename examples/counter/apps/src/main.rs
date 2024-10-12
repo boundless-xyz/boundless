b26ae0eacea685720d98e52aed8612f030a00c3c
Nuke 🌄 <nuke-web3@proton.me>
2024-10-12 11:37:41 -0600

    rename requestor_private_key to generalize: we use one key for any wallet & signing operations

--- examples/counter/apps/src/main.rs
+++ examples/counter/apps/src/main.rs
@@ -43,7 +43,7 @@ struct Args {
     rpc_url: Url,
     /// Private key used to interact with the Counter contract.
     #[clap(long, env)]
-    requestor_private_key: PrivateKeySigner,
+    private_key: PrivateKeySigner,
     /// Address of the Counter contract.
     #[clap(short, long, env)]
     counter_address: Address,
@@ -66,7 +66,7 @@ async fn main() -> Result<()> {
 
     // NOTE: Using a separate `run` function to facilitate testing below.
     run(
-        args.requestor_private_key,
+        args.private_key,
         args.rpc_url,
         args.proof_market_address,
         args.set_verifier_address,
@@ -78,7 +78,7 @@ async fn main() -> Result<()> {
 }
 
 async fn run(
-    requestor_private_key: PrivateKeySigner,
+    private_key: PrivateKeySigner,
     rpc_url: Url,
     proof_market_address: Address,
     set_verifier_address: Address,
@@ -86,7 +86,7 @@ async fn run(
 ) -> Result<()> {
     // Create a Boundless client from the provided parameters.
     let boundless_client = Client::from_parts(
-        requestor_private_key,
+        private_key,
         rpc_url,
         proof_market_address,
         set_verifier_address,
