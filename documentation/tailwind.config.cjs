a89d76923f822901b929950323be77fea449c1f7
Cohan Carpentier <cohan.carpentier@gmail.com>
2024-11-07 21:42:09 -0500

    tailwind support

---
+++ documentation/tailwind.config.cjs
@@ -0,0 +1,9 @@
+const config = {
+  theme: {
+    extend: {},
+  },
+}
+
+config.content = ["./node_modules/@risc0/ui/**/*.{ts,tsx}", './site/**/*.{html,md,mdx,tsx,js,jsx}']
+
+export default config;
