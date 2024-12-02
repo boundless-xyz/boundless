8944292bd845758dc5183de86d925eabd3f8751e
capossele <angelocapossele@gmail.com>
2024-12-02 16:24:31 +0000

    update license

--- crates/guest/assessor/assessor-guest/src/main.rs
+++ crates/guest/assessor/assessor-guest/src/main.rs
@@ -1,18 +1,28 @@
-// Copyright (c) 2024 RISC Zero, Inc.
+// Copyright 2024 RISC Zero, Inc.
 //
-// All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #![no_main]
 #![no_std]
 
 extern crate alloc;
 
-use risc0_aggregation::merkle_root;
 use alloc::{vec, vec::Vec};
 use alloy_primitives::B256;
 use alloy_sol_types::SolValue;
 use assessor::AssessorInput;
 use boundless_market::contracts::AssessorJournal;
+use risc0_aggregation::merkle_root;
 use risc0_zkvm::{
     guest::env,
     sha::{Digest, Digestible},
--- license-check.py
+++ license-check.py
@@ -5,11 +5,21 @@ import os
 from pathlib import Path
 import subprocess
 
-HEADER = """
-// Copyright (c) {YEAR} RISC Zero, Inc.
+HEADER = '''
+// Copyright {YEAR} RISC Zero, Inc.
 //
-// All rights reserved.
-""".strip().splitlines()
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'''.strip().splitlines()
 
 EXTENSIONS = [
     ".cpp",
@@ -20,7 +30,6 @@ EXTENSIONS = [
 
 SKIP_PATHS = [
     # ImageID.sol is automatically generated.
-    str(Path.cwd()) + "/contracts/src/SetBuilderImageID.sol",
     str(Path.cwd()) + "/contracts/src/AssessorImageID.sol",
     str(Path.cwd()) + "/contracts/src/UtilImageID.sol"
 ]
