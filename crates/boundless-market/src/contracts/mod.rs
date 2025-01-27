d41502a1c07fb42f25c2fb3be6e9b35635af4ac4
capossele <angelocapossele@gmail.com>
2025-01-27 23:17:40 +0000

    fix broken link

--- crates/boundless-market/src/contracts/mod.rs
+++ crates/boundless-market/src/contracts/mod.rs
@@ -429,6 +429,7 @@ impl Input {
     /// let input = Input::inline(InputBuilder::new().write(&vec![0x41, 0x41, 0x41, 0x41]).unwrap().encode().unwrap());
     /// ```
     ///
+    /// See [`InputBuilder`][crate::input::InputBuilder] for more details on how to write input data.
     pub fn inline(data: impl Into<Bytes>) -> Self {
         Self { inputType: InputType::Inline, data: data.into() }
     }
