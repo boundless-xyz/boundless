9f7e05f93bfdeee27cbe3d3bebe88ab7bc1a86f6
Victor Graf <victor@risczero.com>
2025-03-06 22:00:31 -0800

    simplify callback logic by removing (somewhat weak) call-once property

--- contracts/test/BoundlessMarket.t.sol
+++ contracts/test/BoundlessMarket.t.sol
@@ -2206,8 +2206,8 @@ contract BoundlessMarketBasicTest is BoundlessMarketTest {
         (fill, assessorReceipt) = createFillAndSubmitRoot(request, APP_JOURNAL, address(testProver));
         boundlessMarket.fulfill(fill, assessorReceipt);
 
-        // Verify callback was not called again
-        assertEq(mockCallback.getCallCount(), 1, "Callback should not be called again");
+        // Verify callback is called again
+        assertEq(mockCallback.getCallCount(), 2, "Callback should be called twice");
 
         expectRequestFulfilled(fill.id);
         testProver.expectBalanceChange(1 ether);
