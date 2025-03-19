967743a19fce7c506fb9962b04a3c78df6b60afd
Wolfgang Welz <welzwo@gmail.com>
2025-03-19 12:06:02 +0100

    use receipt from input in idenity

--- crates/guest/util/identity/src/main.rs
+++ crates/guest/util/identity/src/main.rs
@@ -2,15 +2,24 @@
 //
 // All rights reserved.
 
-//! Verify the receipt claim given as input and commit it.
+//! Verify the receipt given as input and commit to its claim digest.
 
 #![no_main]
 
-use risc0_zkvm::{guest::env, ReceiptClaim};
+use risc0_zkvm::{
+    guest::env,
+    sha::{Digest, Digestible},
+    Receipt,
+};
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    let claim: ReceiptClaim = env::read();
-    env::verify_integrity(&claim).unwrap();
+    let image_id: Digest = env::read();
+    let receipt: Receipt = env::read();
+
+    let claim = receipt.claim().unwrap();
+    receipt.verify(image_id).unwrap();
+
+    env::commit_slice(&claim.digest().as_bytes());
 }
