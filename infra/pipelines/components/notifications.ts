9377005e881e252eeb6a6f9cb540417f00f92ebb
Pote <81638931+willpote@users.noreply.github.com>
2025-04-04 11:51:37 -0400

    Init

---
+++ infra/pipelines/components/notifications.ts
@@ -0,0 +1,77 @@
+import * as aws from '@pulumi/aws';
+import * as pulumi from '@pulumi/pulumi';
+
+export class Notifications extends pulumi.ComponentResource {
+  public snsTopic: aws.sns.Topic;
+
+  constructor(
+    name: string,
+    args: {
+      serviceAccountIds: string[];
+      slackChannelId: pulumi.Output<string>;
+      slackTeamId: pulumi.Output<string>;
+    },
+    opts?: pulumi.ComponentResourceOptions
+  ) {
+    super('pipelines:Notifications', name, args, opts);
+
+    const { serviceAccountIds, slackChannelId: slackChannelIdOutput, slackTeamId: slackTeamIdOutput } = args;
+
+    // Create an IAM Role for AWS Chatbot
+    const chatbotRole = new aws.iam.Role('chatbotRole', {
+      assumeRolePolicy: {
+          Version: '2012-10-17',
+          Statement: [
+              {
+                  Effect: 'Allow',
+                  Principal: {
+                      Service: 'chatbot.amazonaws.com',
+                  },
+                  Action: 'sts:AssumeRole',
+              },
+          ],
+      },
+      managedPolicyArns: [
+        'arn:aws:iam::aws:policy/AmazonSNSFullAccess',
+        'arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess',
+      ],
+    });
+
+    // Create an SNS topic for the alerts
+    this.snsTopic = new aws.sns.Topic("boundless-alerts-topic", { name: "boundless-alerts-topic" });
+
+    // Create a policy that allows the service accounts to publish to the SNS topic
+    const snsTopicPolicy = this.snsTopic.arn.apply(arn => aws.iam.getPolicyDocumentOutput({
+      statements: [{
+          actions: [
+              "SNS:Publish",
+          ],
+          effect: "Allow",
+          principals: [{
+              type: "AWS",
+              identifiers: serviceAccountIds,
+          }],
+          resources: [arn],
+          sid: "Grant publish to service accounts",
+      }],
+    }));
+
+    // Attach the policy to the SNS topic
+    new aws.sns.TopicPolicy("service-accounts-publish-policy", {
+        arn: this.snsTopic.arn,
+        policy: snsTopicPolicy.apply(snsTopicPolicy => snsTopicPolicy.json),
+    });
+
+    // Create a Slack channel configuration for the alerts
+    let slackChannelConfiguration = pulumi.all([slackChannelIdOutput, slackTeamIdOutput])
+      .apply(([slackChannelId, slackTeamId]) => new aws.chatbot.SlackChannelConfiguration("boundless-alerts", {
+        configurationName: "boundless-alerts",
+        iamRoleArn: chatbotRole.arn,
+        slackChannelId: slackChannelId,
+        slackTeamId: slackTeamId,
+        snsTopicArns: [this.snsTopic.arn],
+        loggingLevel: "INFO",
+      }));
+    
+  }
+}
