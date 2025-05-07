410a787ec74841fad3aef0a3f42be5905c814e3b
capossele <angelo@risczero.com>
2025-05-07 10:27:18 +0100

    add indexer

---
+++ infra/indexer/index.ts
@@ -0,0 +1,44 @@
+import * as pulumi from '@pulumi/pulumi';
+import { IndexerInstance } from './components/indexer';
+import { getEnvVar } from '../util';
+
+require('dotenv').config();
+
+export = () => {
+  const config = new pulumi.Config();
+  const stackName = pulumi.getStack();
+  const isDev = stackName === "dev";
+  
+  const ethRpcUrl = isDev ? pulumi.output(getEnvVar("ETH_RPC_URL")) : config.requireSecret('ETH_RPC_URL');
+  const rdsPassword = isDev ? pulumi.output(getEnvVar("RDS_PASSWORD")) : config.requireSecret('RDS_PASSWORD');
+  const chainId = isDev ? getEnvVar("CHAIN_ID") : config.require('CHAIN_ID');
+  
+  const githubTokenSecret = config.getSecret('GH_TOKEN_SECRET');
+  const dockerDir = config.require('DOCKER_DIR');
+  const dockerTag = config.require('DOCKER_TAG');
+  const ciCacheSecret = config.getSecret('CI_CACHE_SECRET');
+  const boundlessAddress = config.require('BOUNDLESS_ADDRESS');
+  const baseStackName = config.require('BASE_STACK');
+  const boundlessAlertsTopicArn = config.get('SLACK_ALERTS_TOPIC_ARN');
+
+  const baseStack = new pulumi.StackReference(baseStackName);
+  const vpcId = baseStack.getOutput('VPC_ID') as pulumi.Output<string>;
+  const privSubNetIds = baseStack.getOutput('PRIVATE_SUBNET_IDS') as pulumi.Output<string[]>;
+  const pubSubNetIds = baseStack.getOutput('PUBLIC_SUBNET_IDS') as pulumi.Output<string[]>;
+
+  const indexer = new IndexerInstance(`indexer`, {
+    chainId,
+    ciCacheSecret,
+    dockerDir,
+    dockerTag,
+    privSubNetIds,
+    pubSubNetIds,
+    githubTokenSecret,
+    boundlessAddress,
+    vpcId,
+    rdsPassword,
+    ethRpcUrl,
+    boundlessAlertsTopicArn,
+  });
+
+};
