2144f2a5d443fa3438e12ca92c1af810c1613eaa
Victor Graf <victor@risczero.com>
2025-05-12 16:25:23 -0700

    add license headers

--- crates/boundless-market/src/request_builder/finalizer.rs
+++ crates/boundless-market/src/request_builder/finalizer.rs
@@ -1,4 +1,16 @@
-#![allow(missing_docs)]
+// Copyright 2025 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use super::{Adapt, Layer, RequestParams};
 use crate::contracts::Input as RequestInput;
@@ -65,9 +77,6 @@ impl Adapt<Finalizer> for RequestParams {
             }
         }
 
-        layer
-            .process((program_url, input, requirements, offer, request_id))
-            .await
-            .map_err(Into::into)
+        layer.process((program_url, input, requirements, offer, request_id)).await
     }
 }
