a783d7a7f9d8701e1926fc1ed747bcf002e3afdc
capossele <angelo@risczero.com>
2025-06-06 15:38:34 +0100

    add indexes to order table

--- crates/slasher/migrations/1_slasher.sql
+++ crates/slasher/migrations/1_slasher.sql
@@ -1,10 +1,13 @@
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
     id TEXT PRIMARY KEY,
     expires_at BIGINT NOT NULL,
     lock_expires_at BIGINT NOT NULL
 );
 
-CREATE TABLE last_block (
+CREATE INDEX IF NOT EXISTS idx_orders_expires_at ON orders(expires_at);
+CREATE INDEX IF NOT EXISTS idx_orders_lock_expires_at ON orders(lock_expires_at);
+
+CREATE TABLE IF NOT EXISTS last_block (
     id INTEGER PRIMARY KEY,
     block TEXT
 )
