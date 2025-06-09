74154bcf43f4c32de313f50afc1831da2c97f030
Austin Abell <austinabell8@gmail.com>
2025-06-09 13:18:05 -0400

    rename for clarity

--- crates/broker/src/config.rs
+++ crates/broker/src/config.rs
@@ -63,7 +63,7 @@ mod defaults {
         240
     }
 
-    pub const fn max_pricing_batch_size() -> u32 {
+    pub const fn max_concurrent_order_pricing() -> u32 {
         4
     }
 }
@@ -183,8 +183,8 @@ pub struct MarketConf {
     /// Maximum number of orders to concurrently work on pricing
     ///
     /// Used to limit pricing tasks spawned to prevent overwhelming the system
-    #[serde(default = "defaults::max_pricing_batch_size")]
-    pub max_pricing_batch_size: u32,
+    #[serde(default = "defaults::max_concurrent_order_pricing")]
+    pub max_concurrent_order_pricing: u32,
 }
 
 impl Default for MarketConf {
@@ -216,7 +216,7 @@ impl Default for MarketConf {
             stake_balance_error_threshold: None,
             max_concurrent_proofs: None,
             cache_dir: None,
-            max_pricing_batch_size: defaults::max_pricing_batch_size(),
+            max_concurrent_order_pricing: defaults::max_concurrent_order_pricing(),
         }
     }
 }
