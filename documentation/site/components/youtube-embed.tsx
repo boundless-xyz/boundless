884069e458b011ded1eb1906dca66f6844c82ac9
Sasha Aldrick <sasha@risczero.com>
2025-07-01 14:17:32 +0100

    add note about base mainnet above video

--- documentation/site/components/youtube-embed.tsx
+++ documentation/site/components/youtube-embed.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import type React from "react";
 
 interface YoutubeEmbedProps {
   videoId: string;
@@ -7,26 +6,28 @@ interface YoutubeEmbedProps {
 
 export const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ videoId }) => {
   return (
-    <div style={{
-      position: 'relative',
-      paddingBottom: '56.25%', // 16:9 aspect ratio
-      height: 0,
-      overflow: 'hidden',
-      maxWidth: '100%',
-      background: '#000',
-      marginBottom: '1.5rem',
-    }}>
+    <div
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%", // 16:9 aspect ratio
+        height: 0,
+        overflow: "hidden",
+        maxWidth: "100%",
+        background: "#000",
+        marginBottom: "1.5rem",
+      }}
+    >
       <iframe
         src={`https://www.youtube.com/embed/${videoId}`}
-        frameBorder="0"
+        title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           left: 0,
-          width: '100%',
-          height: '100%',
+          width: "100%",
+          height: "100%",
         }}
       />
     </div>
