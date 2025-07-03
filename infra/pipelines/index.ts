92d313b8143dba516615f2c4572198947a1c911c
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-07-03 23:53:46 +0300

    Update index.ts

--- infra/pipelines/index.ts
+++ infra/pipelines/index.ts
@@ -39,7 +39,7 @@ const pulumiStateBucket = new PulumiStateBucket("pulumiStateBucket", {
 // Defines the KMS key used to encrypt and decrypt secrets. 
 // Currently, developers logged in as Admin in the Boundless Dev account can encrypt and decrypt secrets.
 // TODO: Only deployment roles should be allowed to decrypt secrets.
-// Staging and prod deployement roles are the only accounts allowed to decrypt secrets.
+// Staging and prod deployment roles are the only accounts allowed to decrypt secrets.
 const pulumiSecrets = new PulumiSecrets("pulumiSecrets", {
   accountId: BOUNDLESS_OPS_ACCOUNT_ID,
   encryptKmsKeyArns: [
