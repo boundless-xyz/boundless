49da8159c60d4d94141a2fa3e3d38b2ce8864922
Victor Graf <victor@risczero.com>
2025-08-30 22:42:38 -0700

    add todos for the first test

--- crates/boundless-cli/tests/povw.rs
+++ crates/boundless-cli/tests/povw.rs
@@ -15,3 +15,13 @@ fn test_prove_update_help() {
         .stdout(predicates::str::contains("prove-update"))
         .stderr("");
 }
+
+#[test]
+fn prove_update_basic() {
+    // 1. Create a temp dir
+    // 2. Make a work receipt, encode it, and save it to the temp dir.
+    // 3. Run the prove-update command to create a new work log with that receipt.
+    // 4. Make another receipt and save it to the temp dir.
+    // 5. Run the prove-update command again to add the new receipt to the log.
+    todo!();
+}
