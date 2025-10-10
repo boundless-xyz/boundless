a8144bb5877495483252d44784bdd82b1618ccac
Richard Howard <rich@risczero.com>
2025-10-10 12:21:52 -0700

    add pathing

--- infra/packer/scripts/setup_nightly.sh
+++ infra/packer/scripts/setup_nightly.sh
@@ -6,6 +6,9 @@ source ~/.cargo/env
 echo "Installing Bento from source..."
 git clone https://github.com/boundless-xyz/boundless.git
 cd boundless/bento
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export NVCC_APPEND_FLAGS="--generate-code arch=compute_75,code=sm_75 --generate-code arch=compute_86,code=sm_86 --generate-code arch=compute_89,code=sm_89 --generate-code arch=compute_120,code=sm_120"
+export PATH=/usr/local/cuda/bin:$PATH
 cargo build --release -F cuda --bin agent
 cargo build --release --bin rest_api
 cargo build --release --bin bento_cli
