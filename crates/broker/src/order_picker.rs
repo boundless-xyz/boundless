c56d0b88a4ca74d687ec60f9e847df306b4b3859
Eric <tu.eric@hotmail.com>
2025-11-13 20:12:05 -0500

    comment

--- crates/broker/src/order_picker.rs
+++ crates/broker/src/order_picker.rs
@@ -812,7 +812,7 @@ where
         let predicate = Predicate::try_from(order.request.requirements.predicate.clone())
             .map_err(|e| OrderPickerErr::RequestError(Arc::new(e.into())))?;
         let eval_data = if is_blake3_groth16_selector(order.request.requirements.selector) {
-            // TODO(ec2): comment on this
+            // These proofs have to have no journal delivery because they cannot be authenticated on chain.
             FulfillmentData::None
         } else {
             FulfillmentData::from_image_id_and_journal(Digest::from_hex(image_id).unwrap(), journal)
