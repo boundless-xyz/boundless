6866332d2ce48b9a36afb8f9ca41ed4e23ce2983
capossele <angelo@risczero.com>
2025-11-25 15:04:18 +0000

    fix

--- crates/povw/src/mint_calculator.rs
+++ crates/povw/src/mint_calculator.rs
@@ -799,7 +799,7 @@ pub mod prover {
                 // max gap. Add a recent block to make sure we will chain to a present value.
                 let latest_block_number =
                     self.provider.get_block_number().await.context("Failed to get block number")?;
-                let patched_block_numbers = PatchedIterator::<_, 8000>::new(
+                let patched_block_numbers = PatchedIterator::<_, 6000>::new(
                     block_numbers.into_iter().chain([latest_block_number - 2]),
                 );
                 Input::build(
