5bcd2b5ca7fc19dbd0decfa2c8b9c4065c3b2629
Eric <tu.eric@hotmail.com>
2025-11-25 16:19:06 -0500

    update rapidsnark docker to pull in the proving key so we can avoid setting variables

--- blake3_groth16/dockerfiles/rapidsnark.dockerfile
+++ blake3_groth16/dockerfiles/rapidsnark.dockerfile
@@ -1,11 +1,10 @@
 # syntax=docker/dockerfile:1.4
-FROM debian:bookworm-slim AS dependencies
-
+FROM rust:1.89.0-bookworm AS dependencies
 WORKDIR /src/
 
 # APT deps
 RUN apt -qq update && \
-  apt install -y -q build-essential cmake libgmp-dev libsodium-dev nasm curl m4 git
+  apt install -y -q build-essential cmake libgmp-dev libsodium-dev nasm curl m4 git protobuf-compiler clang
 
 WORKDIR /src/
 
@@ -22,15 +21,32 @@ RUN git submodule init && \
   make host && \
   cp ./package/bin/prover /usr/local/sbin/rapidsnark
 
+# Build circom-witnesscalc
+RUN git clone https://github.com/iden3/circom-witnesscalc.git && \
+  cd circom-witnesscalc && \
+  git checkout b7ff0ffd9c72c8f60896ce131ee98a35aba96009 && \
+  cargo install --path .
+
+WORKDIR /setup/
+# Download prover files
+ADD https://staging-signal-artifacts.beboundless.xyz/v3/proving/blake3_groth16_artifacts.tar.xz blake3_groth16_artifacts.tar.xz
+RUN tar -xvf blake3_groth16_artifacts.tar.xz && \
+  rm blake3_groth16_artifacts.tar.xz
 
 # Create a final clean image with all the dependencies to perform stark->snark
 FROM debian:bookworm-slim AS prover
-
 RUN apt -qq update && \
   apt install -y -q build-essential cmake libgmp-dev libsodium-dev nasm curl m4
 
 COPY --from=dependencies /usr/local/sbin/rapidsnark /usr/local/sbin/rapidsnark
+COPY --from=dependencies /usr/local/cargo/bin/calc-witness /usr/local/sbin/calc-witness
 
-RUN ulimit -s unlimited
+WORKDIR /setup/
+COPY --from=dependencies /setup/blake3_groth16_artifacts/verify_for_guest_graph.bin .
+COPY --from=dependencies /setup/blake3_groth16_artifacts/verify_for_guest_final.zkey .
 
-ENTRYPOINT ["/usr/local/sbin/rapidsnark"]
+WORKDIR /app/
+COPY scripts/prove.sh prove.sh
+RUN chmod +x /app/prove.sh
+
+ENTRYPOINT ["/app/prove.sh"]
--- blake3_groth16/src/prove/docker.rs
+++ blake3_groth16/src/prove/docker.rs
@@ -14,8 +14,6 @@
 
 use std::{path::Path, process::ExitStatus};
 
-use crate::prove::witgen::calculate_witness_encoded;
-
 use anyhow::{anyhow, bail, Result};
 use risc0_groth16::ProofJson as Groth16ProofJson;
 use std::process::Command;
@@ -24,34 +22,21 @@ pub fn shrink_wrap(
     work_dir: &Path,
     identity_seal_json: serde_json::Value,
 ) -> Result<Groth16ProofJson> {
-    tracing::debug!("blake3 shrink_wrap with rapidsnark");
+    tracing::debug!("blake3 shrink_wrap with docker rapidsnark");
     if !is_docker_installed() {
         bail!("Please install docker first.")
     }
-    let root_dir = std::env::var("BLAKE3_GROTH16_SETUP_DIR");
-    let root_dir = root_dir.as_ref().map(Path::new).expect("must provide BLAKE3_GROTH16_SETUP_DIR");
 
-    let graph_path = root_dir.join("verify_for_guest_graph.bin");
-    let witness_path = work_dir.join("output.wtns");
     let proof_path = work_dir.join("proof.json");
-    let public_path = work_dir.join("public.json");
-
-    let witness_encoded =
-        calculate_witness_encoded(&graph_path, identity_seal_json.to_string().as_str())?;
-    std::fs::write(&witness_path, witness_encoded)?;
+    let input_path = work_dir.join("input.json");
+    std::fs::write(&input_path, identity_seal_json.to_string())?;
 
     let output = Command::new("docker")
         .arg("run")
         .arg("--rm")
         .arg("-v")
         .arg(format!("{}:/mnt", work_dir.display()))
-        .arg("-v")
-        .arg(format!("{}:/setup", root_dir.display()))
-        .arg("rpsnark:latest")
-        .arg("/setup/verify_for_guest_final.zkey")
-        .arg(format!("/mnt/{}", witness_path.file_name().unwrap().to_str().unwrap()))
-        .arg(format!("/mnt/{}", proof_path.file_name().unwrap().to_str().unwrap()))
-        .arg(format!("/mnt/{}", public_path.file_name().unwrap().to_str().unwrap()))
+        .arg("boundless-blake3-g16:latest")
         .output()?;
     if !output.status.success() {
         return Err(error_from_status(output.status));
@@ -75,7 +60,6 @@ fn is_docker_installed() -> bool {
 
 fn error_from_status(status: ExitStatus) -> anyhow::Error {
     let err = anyhow!("docker returned failure exit code: {:?}", status.code());
-
     // Add a hint to the error context for certain error codes.
     match status.code() {
         Some(126) => err.context("This process may not have permission to run containers."),
