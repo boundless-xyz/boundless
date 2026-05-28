75a29e3e33e2c6501f47d9cf6bebb9582dee3ad5
Remi Bernotavicius <bobbobbio@gmail.com>
2026-05-28 16:08:51 -0700

    Add simple RPC system copied from risc0-r0vm

--- crates/multi-zkvm-types/src/lib.rs
+++ crates/multi-zkvm-types/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod rpc;
+
 use async_trait::async_trait;
 use risc0_zkvm::Receipt;
 
